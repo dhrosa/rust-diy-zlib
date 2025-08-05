cef3324c763d6a75d6da461754e547d8bd171358
Diony Rosa <dhrosa@gmail.com>
2025-08-05 03:58:04 -0400

    Stage 2 of huffman tree construction

--- src/code_table.rs
+++ src/code_table.rs
@@ -14,16 +14,45 @@ fn code_length_counts(code_lengths: &[CodeLength]) -> HashMap<CodeLength, u32> {
     counts
 }
 
-#[derive(PartialEq, Eq, Hash)]
+#[derive(PartialEq, Eq, Hash, Clone, Copy)]
 struct Code {
     bits: u32,
     length: u8,
+}
+
+// Test-only covenience method for constructing a Code from a string.
+impl From<&str> for Code {
+    fn from(s: &str) -> Self {
+        Code {
+            bits: u32::from_str_radix(s, 2).unwrap(),
+            length: s.len() as u8,
+        }
+    }
 }
 
 impl fmt::Debug for Code {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
         write!(f, "{:0width$b}", self.bits, width = self.length as usize)
     }
+}
+
+// Step 2 of algorithm from https://datatracker.ietf.org/doc/html/rfc1951#page-9
+fn min_codes_by_length(code_lengths: &[CodeLength]) -> HashMap<CodeLength, Code> {
+    let mut min_codes = HashMap::new();
+    let mut code_bits = 0;
+    let counts = code_length_counts(code_lengths);
+    let max_code_length = *code_lengths.iter().max().unwrap();
+    for length in 1..=max_code_length {
+        code_bits = (code_bits + counts.get(&(length - 1)).unwrap_or(&0)) << 1;
+        min_codes.insert(
+            length,
+            Code {
+                bits: code_bits,
+                length,
+            },
+        );
+    }
+    min_codes
 }
 
 #[cfg(test)]
@@ -43,5 +72,19 @@ mod tests {
     #[test]
     fn test_code() {
         assert_eq!(format!("{:?}", Code { bits: 2, length: 3 }), "010")
+    }
+
+    #[test]
+    fn test_min_codes() {
+        let code_lengths = &[3, 3, 3, 3, 3, 2, 4, 4];
+        assert_eq!(
+            min_codes_by_length(code_lengths),
+            HashMap::from([
+                (1, Code::from("0")),
+                (2, Code::from("00")),
+                (3, Code::from("010")),
+                (4, Code::from("1110")),
+            ])
+        );
     }
 }
