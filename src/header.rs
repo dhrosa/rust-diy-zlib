d8113a45a4242b7c24f11af9e64ef710b0705ee0
Diony Rosa <dhrosa@gmail.com>
2025-08-20 22:10:32 -0400

    Split header into separate module

---
+++ src/header.rs
@@ -0,0 +1,155 @@
+use crate::error::{InflateError, InflateResult};
+
+trait BitRange {
+    fn bits(&self, range: std::ops::RangeInclusive<u8>) -> Self;
+}
+
+impl BitRange for u8 {
+    fn bits(&self, range: std::ops::RangeInclusive<u8>) -> Self {
+        let mask = 0xFF >> (7 - range.end());
+        (self & mask) >> range.start()
+    }
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct CompressionInfo(u8);
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub enum CompressionMethod {
+    Deflate = 8,
+}
+
+impl TryFrom<u8> for CompressionMethod {
+    type Error = InflateError;
+
+    fn try_from(value: u8) -> InflateResult<Self> {
+        if value != CompressionMethod::Deflate as u8 {
+            return Err(InflateError::InvalidCompressionMethod(value));
+        }
+        Ok(CompressionMethod::Deflate)
+    }
+}
+
+impl CompressionInfo {
+    pub fn window_size(&self) -> u16 {
+        let exponent = self.0 + 8;
+        return 1 << exponent;
+    }
+}
+
+impl TryFrom<u8> for CompressionInfo {
+    type Error = InflateError;
+
+    fn try_from(value: u8) -> InflateResult<Self> {
+        if value >= 8 {
+            return Err(InflateError::InvalidCompressionInfo(value));
+        }
+        Ok(CompressionInfo(value))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Flags {
+    pub preset_dictionary: bool,
+    pub compression_level: u8,
+}
+
+impl From<u8> for Flags {
+    fn from(value: u8) -> Self {
+        Self {
+            preset_dictionary: value.bits(5..=5) != 0,
+            compression_level: value.bits(6..=7),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct StreamHeader {
+    pub method: CompressionMethod,
+    pub info: CompressionInfo,
+    pub flags: Flags,
+}
+
+impl TryFrom<&[u8; 2]> for StreamHeader {
+    type Error = InflateError;
+
+    fn try_from(bytes: &[u8; 2]) -> InflateResult<Self> {
+        let [cmf, flg] = *bytes;
+        let method = CompressionMethod::try_from(cmf.bits(0..=3))?;
+        let info = CompressionInfo::try_from(cmf.bits(4..=7))?;
+        let flags = Flags::from(flg);
+
+        let checksum = ((cmf as u16) << 8) + (flg as u16);
+        if checksum % 31 != 0 {
+            return Err(InflateError::FlagCheckMismatch(checksum));
+        }
+        Ok(StreamHeader {
+            method,
+            info,
+            flags,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::InflateError::*;
+    use super::*;
+    use std::assert_matches::assert_matches;
+
+    #[test]
+    fn test_bits() {
+        let val: u8 = 0b1100;
+        assert_eq!(val.bits(0..=1), 0b00);
+        assert_eq!(val.bits(1..=2), 0b10);
+        assert_eq!(val.bits(2..=3), 0b11);
+        assert_eq!(val.bits(3..=4), 0b01);
+        assert_eq!(val.bits(4..=5), 0b00);
+    }
+
+    #[test]
+    fn test_invalid_compression_method() {
+        assert_matches!(
+            StreamHeader::try_from(&[1, 0]),
+            Err(InvalidCompressionMethod(1))
+        );
+    }
+
+    #[test]
+    fn test_invalid_compression_info() {
+        assert_matches!(
+            StreamHeader::try_from(&[0x88, 0]),
+            Err(InvalidCompressionInfo(8))
+        );
+    }
+
+    #[test]
+    fn test_flag_check_mismatch() {
+        assert_matches!(
+            StreamHeader::try_from(&[0x08, 0]),
+            Err(FlagCheckMismatch(0x800)),
+        );
+    }
+
+    #[test]
+    fn test_valid_stream_header() -> InflateResult<()> {
+        let header = StreamHeader::try_from(&[0x48, 0b1010_0000 + 8])?;
+        assert_eq!(
+            header,
+            StreamHeader {
+                method: CompressionMethod::Deflate,
+                info: CompressionInfo(4),
+                flags: Flags {
+                    preset_dictionary: true,
+                    compression_level: 2,
+                }
+            }
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_window_size() {
+        assert_eq!(CompressionInfo(7).window_size(), 32768);
+    }
+}
